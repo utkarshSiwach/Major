acbcf857471fc2c9e98371c370b1516817c9dab4
utkarshSiwach <utkarsh.siwach@gmail.com>
2014-09-19 11:17:39 +0530

    more changes

--- source code/Source.cpp
+++ source code/Source.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include "MySqlCon.h"
+
 using namespace std;
 
 class Teachers;
@@ -12,43 +14,109 @@ public:
 	char * name;
 	Subjects *subArr;
 	Batches * batchArr;
-
+	static void fetchRecordsFromDB();
 	void display();
 
-}oTeachersArr[10];
+};
 
 class Subjects {
 public:
 	int id;
-	char * name;
+	const char * name;
+	static void fetchRecordsFromDB();
 	void display();
-}oSubjectsArr[10];
+};
 
 class Batches {
 public:
 	int id;
 	Students * studentArr;
 	void display();
-}oBatchesArr[10];
+	static void fetchRecordsFromDB();
+};
 
 class Students {
 public:
 	int id;
-	char * name;
+	const char * name;
 	int batchId;
 	Subjects *subArr;
 	void display();
-}oStudentsArr[10];
-void Teachers::display() {
-	cout<<"\nTeaccher id:"<<id<<" name:"<<name;
-	subArr->display();
-	batchArr->display();
+	static void fetchRecordsFromDB();
+};
+
+std::vector<Batches*> batchVector;
+std::vector<Teachers*> teacherVector;
+std::vector<Subjects*> subjectVector;
+std::vector<Students*> studentVector;
+
+void Teachers::fetchRecordsFromDB() {
+	MySqlDatabase oDB;
+	oDB.createConn();
+	ResultSet *rs = oDB.execute("select * from teachers");
+	int n = rs->rowsCount();
+	Teachers *temp;	
+	while(rs->next()) {
+		temp= new Teachers();
+		temp->id = rs->getInt(1);
+		temp->name = new char[40];
+		strcpy_s(temp->name,40,rs->getString(2).c_str());
+		teacherVector.push_back(temp);
+	}
+	delete rs;
+}
+void Subjects::fetchRecordsFromDB() {
+	MySqlDatabase oDB;
+	oDB.createConn();
+	ResultSet *rs = oDB.execute("select * from subjects");
+	int n = rs->rowsCount();
+	Subjects *temp;	
+	while(rs->next()) {
+		temp= new Subjects();
+		temp->id = rs->getInt(1);
+		temp->name = rs->getString(2).c_str();
+		subjectVector.push_back(temp);
+	}
+	delete rs;
+}
+void Batches::fetchRecordsFromDB() {
+	MySqlDatabase oDB;
+	oDB.createConn();
+	ResultSet *rs = oDB.execute("select * from batches");
+	int n = rs->rowsCount();
+	Batches *temp;	
+	while(rs->next()) {
+		temp= new Batches();
+		temp->id = rs->getInt(1);
+		batchVector.push_back(temp);
+	}
+	delete rs;
+}
+void Students::fetchRecordsFromDB() {
+	MySqlDatabase oDB;
+	oDB.createConn();
+	ResultSet *rs = oDB.execute("select * from students");
+	int n = rs->rowsCount();
+	Students *temp;	
+	while(rs->next()) {
+		temp= new Students();
+		temp->id = rs->getInt(1);
+		temp->name = rs->getString(2).c_str();
+		temp->batchId = rs->getInt(3);
+		studentVector.push_back(temp);
+	}
+	delete rs;
 }
 
+void Teachers::display() {
+	cout<<"\nTeaccher id:"<<id<<" name:"<<name;
+	if(subArr!=NULL)subArr->display();
+	if(batchArr!=NULL)batchArr->display();
+	cout<<endl;
+}
 void Subjects::display() {
 	cout<<"\nSubject id:"<<id<<" name:"<<name;
 }
-
 void Batches::display() {
 	cout<<"\nBatch id:"<<id<<" student arr:";
 	studentArr->display();
@@ -57,40 +125,17 @@ void Students::display() {
 	cout<<"\nStudent id:"<<id<<" name:"<<name<<"batchId :"<<batchId<<"subject arr:";
 	subArr->display();
 }
-void main() {
-	char temp[10];
-	for(int i=0;i<10;i++) {
-		oStudentsArr[i].id=i+1;		
-		oStudentsArr[i].name = (char*) malloc(sizeof(char)*10);
-		strcpy_s(temp,"student");
-		temp[7]=(char)(48+i);
-		temp[8]='\0';
-		strcpy_s(oStudentsArr[i].name,sizeof(char)*10,temp);	
-		
-		oSubjectsArr[i].id=i+1;
-		oSubjectsArr[i].name = (char*) malloc(sizeof(char)*10);
-		strcpy_s(temp,"subject");
-		temp[7]=(char)(48+i);
-		temp[8]='\0';
-		strcpy_s(oSubjectsArr[i].name,sizeof(char)*10,temp);
-		oStudentsArr[i].subArr=&oSubjectsArr[i];
-		oBatchesArr[i].id=i+1;
-		oBatchesArr[i].studentArr = &oStudentsArr[i];
 
-		oTeachersArr[i].id=i+1;
-		oTeachersArr[i].name = (char*) malloc(sizeof(char)*10);
-		strcpy_s(temp,"teacher");
-		temp[7]=(char)(48+i);
-		temp[8]='\0';
-		strcpy_s(oTeachersArr[i].name,sizeof(char)*10,temp);
-		oTeachersArr[i].subArr = &oSubjectsArr[i];
-		oTeachersArr[i].batchArr = &oBatchesArr[i];
-		
-		oStudentsArr[i].display();
-		oBatchesArr[i].display();
-		oSubjectsArr[i].display();
-		oTeachersArr[i].display();
-		cout<<"\n";
+void main() {
+
+	Teachers::fetchRecordsFromDB();
+	Students::fetchRecordsFromDB();
+	Subjects::fetchRecordsFromDB();
+	Batches::fetchRecordsFromDB();
+
+	for(int i=0;i<teacherVector.size();i++) {
+		teacherVector[i]->display();
 	}
+	
 	system("pause");
 }
